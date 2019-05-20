f006c8b994eb48fe49402666b645485e08b3ea9c
Miguel A. Ruiz <miguel.ruiz@rci.rogers.com>
2019-05-19 21:38:42 -0400

    Adding script

---
+++ run.sh
@@ -0,0 +1,6 @@
+cat <<END
+This is another thest for jenkins 
+=================================
+Add Operation:
+ Total = ${FIRST_NUMBER} + ${SECOND_NUMBER} = $(($FIRST_NUMBER + $SECOND_NUMBER))
+END
